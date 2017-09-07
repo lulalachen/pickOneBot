5abaee0a48478129ab6e9cd99888c168aaffdd8e
Lulala Chen <lulalachen@gmail.com>
2017-09-07 11:54:23 +0800

    new cat

--- src/app.js
+++ src/app.js
@@ -4,16 +4,19 @@ import R from 'ramda'
 import D from 'date-fp'
 import chalk from 'chalk'
 import fetch from 'isomorphic-fetch'
+import Sandbox from 'sandbox'
 
 import dotenv from 'dotenv'
 
 dotenv.load()
 
+const s = new Sandbox()
+
 const logger = (...textArgs) => {
   const date = D.format('YYYY-MM-DD hh:mm:ss', new Date())
   console.log(`${chalk.gray(`[${date}]`)} - ${textArgs.join(' ')}`)
 }
-logger('gg', 'wefw', 'wefwe')
+
 const { EMAIL, PASSWORD, APP_NAME = 'pickonebot' } = process.env
 if (!EMAIL || !PASSWORD) throw Error('Missing email or password')
 if (!APP_NAME) throw Error('Missing app name')
@@ -51,7 +54,7 @@ const pickOne = (levelText = 'easy') => {
     ))(leetcodeTable)
   const length = R.length(availableQuestions)
   const randomIndex = Math.floor(Math.random() * (length - 1))
-  logger(availableQuestions[randomIndex])
+  // logger(availableQuestions[randomIndex])
   return availableQuestions[randomIndex]
 }
 
@@ -84,6 +87,25 @@ updateLeetcodeTable()
 // const whiteList = ['619160284'] // Lulala Chen
 // const threadIdWhiteList = ['535544279949045', '1245194108'] // LeetCode, hau
 // const myself = '100000631826547'
+
+let sayGoodbye = () => ({})
+const threadsCurrentSession = new Set()
+
+let threadIdLastSession
+try {
+  threadIdLastSession = new Set(JSON.parse(fs.readFileSync('threadIdLastSession.json', 'utf8')))
+} catch (e) {
+  threadIdLastSession = new Set()
+  fs.appendFileSync('threadIdLastSession.json', '[]')
+}
+
+// 1478245778936154 -> SexyCoding
+process.on('SIGINT', () => {
+  fs.writeFileSync('threadIdLastSession.json', JSON.stringify(Array.from(threadsCurrentSession)))
+  sayGoodbye()
+  setTimeout(() => process.exit(), 1000)
+})
+
 let appState
 try {
   appState = JSON.parse(fs.readFileSync('appstate.json', 'utf8'))
@@ -91,6 +113,7 @@ try {
   appState = {}
   fs.appendFileSync('appstate.json', '{}')
 }
+
 
 const credentials = R.ifElse(
   R.equals({}),
@@ -100,20 +123,30 @@ const credentials = R.ifElse(
   }),
   R.objOf('appState')
 )(appState)
-logger('Login with: ')
-logger(appState === {} ? 'email/password' : 'credentials')
+logger(`Login with: ${appState === {} ? 'email/password' : 'credentials'}`)
 
 login(credentials, {
   forceLogin: true,
 }, (err, api) => {
   fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()))
-
+  const sayHello = () => {
+    threadIdLastSession.forEach((threadID) =>
+      api.sendMessage('我又回來啦～喵', threadID)
+    )
+  }
+  sayHello()
+  sayGoodbye = () => {
+    threadsCurrentSession.forEach((threadID) =>
+      api.sendMessage('我要下線啦～喵', threadID)
+    )
+  }
   if (err) return console.error(err)
   api.setOptions({ selfListen: true, logLevel: 'silent' })
   api.listen((error, event = {}) => {
     const currentUserId = api.getCurrentUserID()
     const { type, senderID, body = '', threadID, attachments = [{}] } = event
     const { stickerID = 'No sticker' } = R.pathOr({}, [0])(attachments)
+    threadsCurrentSession.add(threadID)
     logger(type, senderID, body, threadID, stickerID)
     if (stickerID !== 'No sticker' && senderID !== currentUserId) {
       api.sendMessage({ sticker: stickerID }, threadID)
@@ -135,6 +168,17 @@ login(credentials, {
           body: `It's ${difficulty}! \n題目：${questionTitle} \n通過率：${acceptRate} \n大家加油！`,
         }, threadID)
         api.sendMessage({ sticker: '1398251827059667' }, threadID)
+        break
+      }
+      case '/sandbox': {
+        const code = body.replace('/sandbox')
+        api.sendMessage('正在努力計算！喵', threadID)
+        const endTyping = api.sendTypingIndicator(threadID, logger)
+        s.run(code, ({ result, console }) => {
+          endTyping()
+          api.sendMessage(`結果：${result}`, threadID)
+          api.sendMessage(`Log：${console.join(',')}`, threadID)
+        })
         break
       }
       default:
